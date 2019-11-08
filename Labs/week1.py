39809e0dacac72d36c266fce0d42bee11e571ee6
saaish <vikramv1989@gmail.com>
2019-11-08 13:05:01 -0500

    new module

--- Labs/week1.py
+++ Labs/week1.py
@@ -7,7 +7,17 @@ age = int(input("What is your age?"))
 def calculateyear():
     today = int(datetime.now().strftime('%Y'))
 #    yearborn = today - age
-    print("Your birth year is " + str(today - age)) 
+    print("Your birth year is " + str(today - age))
+def note():
+    if age < 18:
+       print("You are too young to vote, Bud") 
+    elif age < 21:
+       print("Pal!You are old enough to vote,but too young to drink")
+    elif 21 < age < 65:
+       print("Great job!You are old enough to vote and drink")
+    else:
+       print("You qualify for Social Security")
 name()
 calculateyear()
+note()
 
